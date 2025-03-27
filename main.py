e693f455e57643bdcb5461d6e6ed9771f9b79e20
Lucas Eidson <leidson@u.rochester.edu>
2025-03-27 16:38:48 -0400

    Add file input and testing resources

--- main.py
+++ main.py
@@ -4,13 +4,12 @@ A query can be answered in a bayes net by computing sums of products of conditio
 
 """ Rejection Sampling: """
 """ Gibbs sampling: """
-
 """TODO: figure out how to index P(X | Y,Z,... ) - Check Discord, then move on to exact inference"""
 
 
 def main():
-    vars = get_bayesian_net()
-    print("____________________")
+    input = open("easy1.txt").readlines()
+    vars = get_bayesian_net(input)
     for v in vars.keys():
         print(v)
         cpt = vars[v].cpt
@@ -26,17 +25,22 @@ def main():
     print(cpt[2][1])
 
 
-def get_bayesian_net():
-    num_vars = int(input())
+def get_bayesian_net(lines):
+    index = 0
+    num_vars = int(lines[index])
+    index += 1
     vars = dict()
     for i in range(num_vars):
-        var_info = input().split()
+        var_info = lines[index].split()
+        index += 1
         new_var = variable(var_info[0], var_info[1:])
         vars[new_var.name] = new_var
-    num_tables = int(input())
+    num_tables = int(lines[index])
+    index += 1
     for _ in range(num_tables):
-        input()  # Skip the blank line in-between each table
-        cpt_vars = input().split()
+        index += 1  # Skip the blank line in-between each table
+        cpt_vars = lines[index].split()
+        index += 1
         num_cpt_vars = len(cpt_vars)
         # Transform CPT Vars to variable obj instead of strings:
         for i in range(num_cpt_vars):
@@ -46,7 +50,8 @@ def get_bayesian_net():
             cpt_vars[0].addParents(cpt_vars[1:])
         cpt = list()
         for _ in range(2 ** (num_cpt_vars - 1)):
-            vals = input().split()
+            vals = lines[index].split()
+            index += 1
             for i in range(len(vals)):
                 vals[i] = float(vals[i])
             cpt.append(vals)
